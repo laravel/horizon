d6435c431f4738447c164d200f2c7c07bd118109
Charles Peterson <cpeterson@brokerbin.com>
2017-10-10 11:42:06 -0500

    this displays properly without blowing away relevant data.

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -24,10 +24,9 @@ class Controller extends BaseController
      */
     public function checkPayload(&$job) {
         $payload_size = ini_get('mbstring.func_overload') ? mb_strlen($job->payload , '8bit') : strlen($job->payload);
+        $job->payload = json_decode($job->payload);
         if($payload_size > config('horizon.payload')) {
-            $job->payload = 'Payload Size Exceeded';
-        } else {
-            $job->payload = json_decode($job->payload);
+            $job->payload->data = 'Payload Size Exceeded';
         }
     }
 }
