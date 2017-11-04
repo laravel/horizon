a7800e40a415b92d8d4b1267f5c416656311c7e2
Taylor Otwell <taylor@laravel.com>
2017-11-04 13:11:20 -0500

    Update Horizon.php

--- src/Horizon.php
+++ src/Horizon.php
@@ -88,7 +88,7 @@ class Horizon
     public static function use($connection)
     {
         if (is_null($config = config("database.redis.{$connection}"))){
-            throw new Exception("Redis connection '{$connection}' is invalid.");
+            throw new Exception("Redis connection [{$connection}] has not been configured.");
         }
 
         config(['database.redis.horizon' => array_merge($config, [
