eb84207e527023a542a31643118fe6d00d197e36
Dries Vints <dries.vints@gmail.com>
2019-01-30 16:02:50 +0100

    Fix method signature

--- tests/Feature/Fixtures/FakeSupervisorFactory.php
+++ tests/Feature/Fixtures/FakeSupervisorFactory.php
@@ -3,13 +3,14 @@
 namespace Laravel\Horizon\Tests\Feature\Fixtures;
 
 use Laravel\Horizon\SupervisorFactory;
+use Laravel\Horizon\SupervisorOptions;
 use Laravel\Horizon\Tests\Feature\Fakes\SupervisorWithFakeMonitor;
 
 class FakeSupervisorFactory extends SupervisorFactory
 {
     public $supervisor;
 
-    public function make($options)
+    public function make(SupervisorOptions $options)
     {
         return $this->supervisor = new SupervisorWithFakeMonitor($options);
     }
