f1f830e8de84c2827c9e155a6abd5cc4576b498e
Taylor Otwell <taylor@laravel.com>
2019-02-15 08:14:01 -0600

    adjust configuration for 5.8 default configuration

--- src/Horizon.php
+++ src/Horizon.php
@@ -87,7 +87,9 @@ class Horizon
      */
     public static function use($connection)
     {
-        if (is_null($config = config("database.redis.{$connection}"))) {
+        if (! is_null($config = config("database.redis.clusters.{$connection}.0"))) {
+            config(["database.redis.{$connection}" => $config]);
+        } elseif (is_null($config) && is_null($config = config("database.redis.{$connection}"))) {
             throw new Exception("Redis connection [{$connection}] has not been configured.");
         }
 
