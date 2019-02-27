5402583fc715b080027c48123ed6cc6ce51436f3
Mohamed Said <themohamedsaid@gmail.com>
2019-02-27 13:09:07 +0200

    update config file

--- config/horizon.php
+++ config/horizon.php
@@ -2,6 +2,17 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Horizon Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Horizon will be accessible from. Feel free
+    | to change this path to anything you like. Note that the URI will not
+    | affect the paths of its internal API that aren't exposed to users.
+    |
+    */
+
     'path' => 'horizon',
 
     /*
