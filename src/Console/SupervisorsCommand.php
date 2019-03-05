e9febd480637adb5cf41a9ac1e8a8efe00b6df39
Set Kyar Wa Lar <setkyar16@gmail.com>
2019-03-06 01:19:33 +0800

    Add individual/ping option to supervisor command

--- src/Console/SupervisorsCommand.php
+++ src/Console/SupervisorsCommand.php
@@ -12,7 +12,7 @@ class SupervisorsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'horizon:supervisors';
+    protected $signature = 'horizon:supervisors {--individual=} {--ping}';
 
     /**
      * The console command description.
@@ -24,20 +24,76 @@ class SupervisorsCommand extends Command
     /**
      * Execute the console command.
      *
-     * @param  \Laravel\Horizon\Contracts\SupervisorRepository  $supervisors
+     * @param  \Laravel\Horizon\Contracts\SupervisorRepository  $supervisorsRepository
      * @return void
      */
-    public function handle(SupervisorRepository $supervisors)
+    public function handle(SupervisorRepository $supervisorsRepository)
     {
-        $supervisors = $supervisors->all();
+        $supervisors = collect($supervisorsRepository->all());
 
-        if (empty($supervisors)) {
+        if ($supervisors->count() == 0) {
             return $this->info('No supervisors are running.');
+        }
+
+        if ($this->option('ping')) {
+            $activeRunningSupervisor = $supervisors->where('status', 'running')->count();
+
+            return $this->info('Total active running supervisors ' . $activeRunningSupervisor);
+        }
+
+        $individualSupervisorName = $this->option('individual');
+
+        if ($individualSupervisorName) {
+            $individualSupervisor = $supervisorsRepository->find($individualSupervisorName);
+
+            if ($individualSupervisor) {
+                $headers = [
+                            'Name',
+                            'PID', 
+                            'Status',
+                            'Workers',
+                            'Balancing',
+                            'Connection',
+                            'Queue',
+                            'Delay',
+                            'Force',
+                            'Max Processes',
+                            'Min Processes',
+                            'Max Tries',
+                            'Memory',
+                            'Sleep',
+                            'Timeout'
+                        ];
+
+                $data[]  = [
+                    $individualSupervisor->name,
+                    $individualSupervisor->pid,
+                    $individualSupervisor->status,
+                    collect($individualSupervisor->processes)->map(function ($count, $queue) {
+                        return $queue.' ('.$count.')';
+                    })->implode(', '),
+                    $individualSupervisor->options['balance'],
+                    $individualSupervisor->options['connection'],
+                    $individualSupervisor->options['queue'],
+                    $individualSupervisor->options['delay'],
+                    $individualSupervisor->options['force'],
+                    $individualSupervisor->options['maxProcesses'],
+                    $individualSupervisor->options['minProcesses'],
+                    $individualSupervisor->options['maxTries'],
+                    $individualSupervisor->options['memory'],
+                    $individualSupervisor->options['sleep'],
+                    $individualSupervisor->options['timeout'],
+                ];
+
+                return $this->table($headers, $data);
+            }
+
+            return $this->error($individualSupervisorName . ' not found!');
         }
 
         $this->table([
             'Name', 'PID', 'Status', 'Workers', 'Balancing',
-        ], collect($supervisors)->map(function ($supervisor) {
+        ], $supervisors->map(function ($supervisor) {
             return [
                 $supervisor->name,
                 $supervisor->pid,
