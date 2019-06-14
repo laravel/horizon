4144aa18a14181ad93360889a26c3e7822d5745d
Dries Vints <dries.vints@gmail.com>
2019-06-14 14:28:45 +0200

    Remove unnecessary type attribute

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Style sheets-->
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="{{ asset(mix($cssFile, 'vendor/horizon')) }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset(mix($cssFile, 'vendor/horizon')) }}" rel="stylesheet">
 </head>
 <body>
 <div id="horizon" v-cloak>
