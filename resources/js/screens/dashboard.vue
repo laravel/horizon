fb9df5bbc5eb8a90f7e4ba4756994fafa0311230
Dries Vints <dries.vints@gmail.com>
2019-06-14 14:03:19 +0200

    Revert "[3.0] Display worker CPU and memory utilization in supervisor list"

--- resources/js/screens/dashboard.vue
+++ resources/js/screens/dashboard.vue
@@ -300,8 +300,6 @@
                 <tr>
                     <th>Supervisor</th>
                     <th>Processes</th>
-                    <th>CPU Threads</th>
-                    <th>Memory</th>
                     <th>Queues</th>
                     <th class="text-right">Balancing</th>
                 </tr>
@@ -311,8 +309,6 @@
                 <tr v-for="supervisor in worker.supervisors">
                     <td>{{ superVisorDisplayName(supervisor.name, worker.name) }}</td>
                     <td>{{ countProcesses(supervisor.processes) }}</td>
-                    <td>{{ supervisor.cpu }}</td>
-                    <td>{{ supervisor.mem }}%</td>
                     <td>{{ supervisor.options.queue.replace(/,/g, ', ') }}</td>
                     <td class="text-right">
                         ({{ supervisor.options.balance.charAt(0).toUpperCase() + supervisor.options.balance.slice(1) }})
