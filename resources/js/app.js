0e0299076b636376994b7f19c6f7372958c59d6b
Sander Dekkers <sander.dekkers@port-it.nl>
2019-08-05 15:50:06 +0200

    Forcing Vue to use the X-Requested-With header, set to XMLHttpRequest so Laravel can detect the request as Ajax, so AJAX requests do not get updated in the Laravel Session.

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,8 @@ require('bootstrap');
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 }
