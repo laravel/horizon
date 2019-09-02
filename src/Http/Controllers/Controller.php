2e0ed27472ffa80d677d175557da6850698a859f
zloblin <zloblin@zivitech.com>
2019-09-02 16:43:32 +0300

    filter nested fields

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -8,7 +8,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    protected $heavyFialds = ['payload', 'exception'];
     /**
      * Create a new controller instance.
      *
@@ -22,9 +21,8 @@ class Controller extends BaseController
     protected function filterHeavyFields(Collection $collection)
     {
         return $collection->transform(function ($item, $key) {
-            foreach ($this->heavyFialds as $field) {
-                $item->{$field} = json_encode([]);
-            }
+            $item->payload->data = [];
+            $item->exception = [];
             return $item;
         });
     }
