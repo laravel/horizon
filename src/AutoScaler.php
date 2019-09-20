48fc4c2c4251b7475156beb903410b0d84f12293
Lasse Rafn <lasserafn@gmail.com>
2019-09-20 13:27:59 +0100

    Will set to min-workers when using `simple` balancing, and there is no jobs available.

--- src/AutoScaler.php
+++ src/AutoScaler.php
@@ -8,136 +8,140 @@ use Laravel\Horizon\Contracts\MetricsRepository;
 
 class AutoScaler
 {
-    /**
-     * The queue factory implementation.
-     *
-     * @var \Illuminate\Contracts\Queue\Factory
-     */
-    public $queue;
+	/**
+	 * The queue factory implementation.
+	 *
+	 * @var \Illuminate\Contracts\Queue\Factory
+	 */
+	public $queue;
 
-    /**
-     * The metrics repository implementation.
-     *
-     * @var \Laravel\Horizon\Contracts\MetricsRepository
-     */
-    public $metrics;
+	/**
+	 * The metrics repository implementation.
+	 *
+	 * @var \Laravel\Horizon\Contracts\MetricsRepository
+	 */
+	public $metrics;
 
-    /**
-     * Create a new auto-scaler instance.
-     *
-     * @param  \Illuminate\Contracts\Queue\Factory  $queue
-     * @param  \Laravel\Horizon\Contracts\MetricsRepository  $metrics
-     * @return void
-     */
-    public function __construct(QueueFactory $queue, MetricsRepository $metrics)
-    {
-        $this->queue = $queue;
-        $this->metrics = $metrics;
-    }
+	/**
+	 * Create a new auto-scaler instance.
+	 *
+	 * @param  \Illuminate\Contracts\Queue\Factory  $queue
+	 * @param  \Laravel\Horizon\Contracts\MetricsRepository  $metrics
+	 * @return void
+	 */
+	public function __construct(QueueFactory $queue, MetricsRepository $metrics)
+	{
+		$this->queue = $queue;
+		$this->metrics = $metrics;
+	}
 
-    /**
-     * Balance the workers on the given supervisor.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @return void
-     */
-    public function scale(Supervisor $supervisor)
-    {
-        $pools = $this->poolsByQueue($supervisor);
+	/**
+	 * Balance the workers on the given supervisor.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @return void
+	 */
+	public function scale(Supervisor $supervisor)
+	{
+		$pools = $this->poolsByQueue($supervisor);
 
-        $workers = $this->numberOfWorkersPerQueue(
-            $supervisor, $this->timeToClearPerQueue($supervisor, $pools)
-        );
+		$workers = $this->numberOfWorkersPerQueue(
+			$supervisor, $this->timeToClearPerQueue($supervisor, $pools)
+		);
 
-        $workers->each(function ($workers, $queue) use ($supervisor, $pools) {
-            $this->scalePool($supervisor, $pools[$queue], $workers);
-        });
-    }
+		$workers->each(function ($workers, $queue) use ($supervisor, $pools) {
+			$this->scalePool($supervisor, $pools[$queue], $workers);
+		});
+	}
 
-    /**
-     * Get the process pools keyed by their queue name.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @return \Illuminate\Support\Collection
-     */
-    protected function poolsByQueue(Supervisor $supervisor)
-    {
-        return $supervisor->processPools->mapWithKeys(function ($pool) {
-            return [$pool->queue() => $pool];
-        });
-    }
+	/**
+	 * Get the process pools keyed by their queue name.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @return \Illuminate\Support\Collection
+	 */
+	protected function poolsByQueue(Supervisor $supervisor)
+	{
+		return $supervisor->processPools->mapWithKeys(function ($pool) {
+			return [$pool->queue() => $pool];
+		});
+	}
 
-    /**
-     * Get the times in milliseconds needed to clear the queues.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @param  \Illuminate\Support\Collection  $pools
-     * @return \Illuminate\Support\Collection
-     */
-    protected function timeToClearPerQueue(Supervisor $supervisor, Collection $pools)
-    {
-        return $pools->mapWithKeys(function ($pool, $queue) use ($supervisor) {
-            $size = $this->queue->connection($supervisor->options->connection)->readyNow($queue);
+	/**
+	 * Get the times in milliseconds needed to clear the queues.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @param  \Illuminate\Support\Collection  $pools
+	 * @return \Illuminate\Support\Collection
+	 */
+	protected function timeToClearPerQueue(Supervisor $supervisor, Collection $pools)
+	{
+		return $pools->mapWithKeys(function ($pool, $queue) use ($supervisor) {
+			$size = $this->queue->connection($supervisor->options->connection)->readyNow($queue);
 
-            return [$queue => ($size * $this->metrics->runtimeForQueue($queue))];
-        });
-    }
+			return [$queue => ($size * $this->metrics->runtimeForQueue($queue))];
+		});
+	}
 
-    /**
-     * Get the number of workers needed per queue for proper balance.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @param  \Illuminate\Support\Collection  $timeToClear
-     * @return \Illuminate\Support\Collection
-     */
-    protected function numberOfWorkersPerQueue(Supervisor $supervisor, Collection $timeToClear)
-    {
-        $timeToClearAll = $timeToClear->sum();
+	/**
+	 * Get the number of workers needed per queue for proper balance.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @param  \Illuminate\Support\Collection  $timeToClear
+	 * @return \Illuminate\Support\Collection
+	 */
+	protected function numberOfWorkersPerQueue(Supervisor $supervisor, Collection $timeToClear)
+	{
+		$timeToClearAll = $timeToClear->sum();
 
-        return $timeToClear->mapWithKeys(function ($timeToClear, $queue) use ($supervisor, $timeToClearAll) {
-            if ($timeToClearAll > 0 &&
-                $supervisor->options->autoScaling()) {
-                return [$queue => (($timeToClear / $timeToClearAll) * $supervisor->options->maxProcesses)];
-            } elseif ($timeToClearAll == 0 &&
-                      $supervisor->options->autoScaling()) {
-                return [$queue => $supervisor->options->minProcesses];
-            }
+		return $timeToClear->mapWithKeys(function ($timeToClear, $queue) use ($supervisor, $timeToClearAll) {
+			if ($timeToClearAll > 0 &&
+			    $supervisor->options->autoScaling()) {
+				return [$queue => (($timeToClear / $timeToClearAll) * $supervisor->options->maxProcesses)];
+			} elseif ($timeToClearAll == 0 &&
+			          $supervisor->options->autoScaling()) {
+				return [$queue => $supervisor->options->minProcesses];
+			}
 
-            return [$queue => $supervisor->options->maxProcesses / count($supervisor->processPools)];
-        })->sort();
-    }
+			if($this->queue->connection($supervisor->options->connection)->readyNow($queue) === 0) {
+				return [$queue => $supervisor->options->minProcesses];
+			}
 
-    /**
-     * Scale the given pool to the recommended number of workers.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @param  \Laravel\Horizon\ProcessPool  $pool
-     * @param  float  $workers
-     * @return void
-     */
-    protected function scalePool(Supervisor $supervisor, $pool, $workers)
-    {
-        $supervisor->pruneTerminatingProcesses();
+			return [$queue => $supervisor->options->maxProcesses / count($supervisor->processPools)];
+		})->sort();
+	}
 
-        $poolProcesses = $pool->totalProcessCount();
+	/**
+	 * Scale the given pool to the recommended number of workers.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @param  \Laravel\Horizon\ProcessPool  $pool
+	 * @param  float  $workers
+	 * @return void
+	 */
+	protected function scalePool(Supervisor $supervisor, $pool, $workers)
+	{
+		$supervisor->pruneTerminatingProcesses();
 
-        if (ceil($workers) > $poolProcesses &&
-            $this->wouldNotExceedMaxProcesses($supervisor)) {
-            $pool->scale($poolProcesses + 1);
-        } elseif (ceil($workers) < $poolProcesses &&
-                  $poolProcesses > $supervisor->options->minProcesses) {
-            $pool->scale($poolProcesses - 1);
-        }
-    }
+		$poolProcesses = $pool->totalProcessCount();
 
-    /**
-     * Determine if adding another process would exceed max process limit.
-     *
-     * @param  \Laravel\Horizon\Supervisor  $supervisor
-     * @return bool
-     */
-    protected function wouldNotExceedMaxProcesses(Supervisor $supervisor)
-    {
-        return ($supervisor->totalProcessCount() + 1) <= $supervisor->options->maxProcesses;
-    }
+		if (ceil($workers) > $poolProcesses &&
+		    $this->wouldNotExceedMaxProcesses($supervisor)) {
+			$pool->scale($poolProcesses + 1);
+		} elseif (ceil($workers) < $poolProcesses &&
+		          $poolProcesses > $supervisor->options->minProcesses) {
+			$pool->scale($poolProcesses - 1);
+		}
+	}
+
+	/**
+	 * Determine if adding another process would exceed max process limit.
+	 *
+	 * @param  \Laravel\Horizon\Supervisor  $supervisor
+	 * @return bool
+	 */
+	protected function wouldNotExceedMaxProcesses(Supervisor $supervisor)
+	{
+		return ($supervisor->totalProcessCount() + 1) <= $supervisor->options->maxProcesses;
+	}
 }
