1532f9c32d9739a1886357108fe5c4e1dc9b8e78
Taylor Otwell <taylor@laravel.com>
2019-10-18 08:23:11 -0500

    formatting

--- src/Jobs/StopMonitoringTag.php
+++ src/Jobs/StopMonitoringTag.php
@@ -36,13 +36,14 @@ class StopMonitoringTag
     {
         $tags->stopMonitoring($this->tag);
 
-        $tagJobs = $tags->paginate($this->tag);
+        $monitored = $tags->paginate($this->tag);
 
-        while (count($tagJobs) !== 0) {
-            $jobs->deleteMonitored($tagJobs);
+        while (count($monitored) > 0) {
+            $jobs->deleteMonitored($monitored);
 
-            $offset = array_keys($tagJobs)[count($tagJobs) - 1] + 1;
-            $tagJobs = $tags->paginate($this->tag, $offset);
+            $offset = array_keys($monitored)[count($monitored) - 1] + 1;
+
+            $monitored = $tags->paginate($this->tag, $offset);
         }
 
         $tags->forget($this->tag);
