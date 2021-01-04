2bd8399dd783b85721d572c4268fb188b4f7ed97
Mohamed Said <themohamedsaid@gmail.com>
2021-01-04 16:42:10 +0200

    set lastPushed when executing the delayed enqueue closure not before (#951)

--- src/RedisQueue.php
+++ src/RedisQueue.php
@@ -41,9 +41,17 @@ class RedisQueue extends BaseQueue
      */
     public function push($job, $data = '', $queue = null)
     {
-        $this->lastPushed = $job;
+        return $this->enqueueUsing(
+            $job,
+            $this->createPayload($job, $this->getQueue($queue), $data),
+            $queue,
+            null,
+            function ($payload, $queue) use ($job) {
+                $this->lastPushed = $job;
 
-        return parent::push($job, $data, $queue);
+                return $this->pushRaw($payload, $queue);
+            }
+        );
     }
 
     /**
