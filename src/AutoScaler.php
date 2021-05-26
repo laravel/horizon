c1137b38106403f944e1380a31ad86453d56a0d4
dave <dave@umbrellio.biz>
2021-05-26 10:37:55 +0300

    Revert "asd"
    This reverts commit cf5f0eb7

--- src/AutoScaler.php
+++ src/AutoScaler.php
@@ -11,7 +11,7 @@ class AutoScaler
     /**
      * The queue factory implementation.
      *
-     * @var \Illuminate\Contracts\Queue\Factfory
+     * @var \Illuminate\Contracts\Queue\Factory
      */
     public $queue;
 
