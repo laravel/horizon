93f5d74e1b6126d0de4416c230cb35aaddbe51bc
nthsky <nitianhua@qq.com>
2022-07-12 02:08:25 +0800

    when register, keep the original redis options, only overwrite prefix (#1161)
    * when register horizon, keep the original redis options, only overwrite prefix
    
    * delete space
    
    * Update Horizon.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Horizon.php
+++ src/Horizon.php
@@ -103,9 +103,9 @@ class Horizon
             throw new Exception("Redis connection [{$connection}] has not been configured.");
         }
 
-        config(['database.redis.horizon' => array_merge($config, [
-            'options' => ['prefix' => config('horizon.prefix') ?: 'horizon:'],
-        ])]);
+        $config['options']['prefix'] = config('horizon.prefix') ?: 'horizon:';
+
+        config(['database.redis.horizon' => $config]);
     }
 
     /**
