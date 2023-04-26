a207be7951224ff1fd0d62ebf05170b53f6da09a
Anthony Cooper <AnthonyCooper.dev@gmail.com>
2023-04-26 16:51:37 +0100

    Fix deprecation errors for PHP 8.2

--- src/Contracts/MetricsRepository.php
+++ src/Contracts/MetricsRepository.php
@@ -82,7 +82,7 @@ interface MetricsRepository
      * Increment the metrics information for a job.
      *
      * @param  string  $job
-     * @param  float  $runtime
+     * @param  float|null  $runtime
      * @return void
      */
     public function incrementJob($job, $runtime);
@@ -91,7 +91,7 @@ interface MetricsRepository
      * Increment the metrics information for a queue.
      *
      * @param  string  $queue
-     * @param  float  $runtime
+     * @param  float|null  $runtime
      * @return void
      */
     public function incrementQueue($queue, $runtime);
