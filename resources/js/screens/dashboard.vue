ea1ff821f10a3c4ace5a48cba76405e1a01d95d8
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-06-22 02:46:13 +0200

    Make inactive supervisors visible (#1286)

--- resources/js/screens/dashboard.vue
+++ resources/js/screens/dashboard.vue
@@ -337,6 +337,9 @@
                         <svg v-if="supervisor.status == 'paused'" class="fill-warning mr-1" viewBox="0 0 20 20" style="width: 1rem; height: 1rem;">
                             <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM7 6h2v8H7V6zm4 0h2v8h-2V6z"/>
                         </svg>
+                        <svg v-if="supervisor.status == 'inactive'" class="fill-danger mr-1" viewBox="0 0 20 20" style=" width: 1rem; height: 1rem;">
+                            <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z"/>
+                        </svg>
                         {{ superVisorDisplayName(supervisor.name, worker.name) }}
                     </td>
                     <td class="text-muted">{{ supervisor.options.queue.replace(/,/g, ', ') }}</td>
