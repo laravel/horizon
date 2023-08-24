05abc3a9aadf9bac7a3254de4c0760f9afeedc2a
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2023-08-24 15:55:10 -0300

    wip

--- src/WithFeedbacks.php
+++ src/WithFeedbacks.php
@@ -2,15 +2,66 @@
 
 namespace Laravel\Horizon;
 
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Queue\Jobs\RedisJob;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Stringable;
 
 trait WithFeedbacks
 {
 
-    protected function mergeFeedbacksToPayload(array $feedback): void
+    public function afterFeedback(mixed $onSuccess = null, mixed $onFailure = null): void
     {
-        /** @var \Illuminate\Queue\Jobs\RedisJob $job */
+        $job = $this->job;
+        if (!$job instanceof RedisJob) {
+            return;
+        }
+        $backtrace = debug_backtrace();
+        if ($onSuccess !== null) {
+            Queue::after(function (JobProcessed $e) use ($backtrace, $job, $onSuccess) {
+                if ($e->job->uuid() === $job->uuid()) {
+                    $this->mergeFeedbacksToPayload($backtrace, $onSuccess);
+                }
+            });
+        }
+        if ($onFailure !== null) {
+            Queue::failing(function (JobFailed $e) use ($backtrace, $job, $onFailure) {
+                if ($e->job->uuid() === $job->uuid()) {
+                    $this->mergeFeedbacksToPayload($backtrace, $onFailure);
+                }
+            });
+        }
+    }
+
+    public function feedback(mixed $feedback): void
+    {
+        $this->mergeFeedbacksToPayload(debug_backtrace(), $feedback);
+    }
+
+    public function feedbackIf(bool $condition, mixed $feedback): void
+    {
+        if ($condition) {
+            $this->mergeFeedbacksToPayload(debug_backtrace(), $feedback);
+        }
+    }
+
+    protected function getFeedbackCaller(array $backtrace): string
+    {
+        $caller = array_shift($backtrace);
+        $file = Arr::get($caller, 'file', 'unknown');
+        $line = Arr::get($caller, 'line', false);
+        return str($file)
+            ->whenStartsWith(base_path(), function (Stringable $str) {
+                return $str->remove(base_path() . DIRECTORY_SEPARATOR);
+            })
+            ->append($line ? ':' . $line : '')
+            ->toString();
+    }
+
+    protected function mergeFeedbacksToPayload(array $backtrace, mixed $feedback): void
+    {
         $job = $this->job;
         if (!$job instanceof RedisJob) {
             return;
@@ -23,7 +74,11 @@ trait WithFeedbacks
         }
         $payload = json_decode($connection->hMGet($uuid, ['payload'])[0], true);
         $feedbacks = collect(Arr::get($payload, 'feedbacks', []));
-        $payload['feedbacks'] = $feedbacks->push($feedback)->toArray();
+        $payload['feedbacks'] = $feedbacks->push([
+            'content' => $feedback,
+            'time' => strval(microtime(true)),
+            'where' => $this->getFeedbackCaller($backtrace),
+        ])->toArray();
         $connection->hmset($uuid, ['payload' => json_encode($payload)]);
     }
 }
