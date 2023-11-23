bdf58c84b592b83f62262cc6ca98b0debbbc308b
TomaszOnePilot <114939186+TomaszOnePilot@users.noreply.github.com>
2023-11-23 16:47:58 +0100

    fix: undefined array key 'queue' (#1351)
    * fix: undefined array key 'queue'
    
    * Update MasterSupervisorController.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Http/Controllers/MasterSupervisorController.php
+++ src/Http/Controllers/MasterSupervisorController.php
@@ -33,7 +33,7 @@ class MasterSupervisorController extends Controller
                                 'status' => 'inactive',
                                 'processes' => [],
                                 'options' => [
-                                    'queue' => (array_key_exists('queue', $value) && is_array($value['queue']) ? implode(',', $value['queue']) : $value['queue']) ?? '',
+                                    'queue' => array_key_exists('queue', $value) && is_array($value['queue']) ? implode(',', $value['queue']) : ($value['queue'] ?? ''),
                                     'balance' => $value['balance'] ?? null,
                                 ],
                             ];
