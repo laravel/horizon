2a507c0c81700dfab768860d6d7ae6281224d23b
Tim MacDonald <hello@timacdonald.me>
2024-03-19 11:02:11 +1100

    lint

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -54,7 +54,6 @@ class InstallCommand extends Command
         $namespace = Str::replaceLast('\\', '', $this->laravel->getNamespace());
 
         if (file_exists($this->laravel->bootstrapPath('providers.php'))) {
-            // @phpstan-ignore-next-line
             ServiceProvider::addProviderToBootstrapFile("{$namespace}\\Providers\\HorizonServiceProvider");
         } else {
             $appConfig = file_get_contents(config_path('app.php'));
