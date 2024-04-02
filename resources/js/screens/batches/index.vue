c2f26c8ed295abf4a814e6c1e0c674cb3d05c764
Markus Machatschek <markus.machatschek@hey.com>
2024-04-02 18:27:58 +0200

    utility changes (rebase changes)

--- resources/js/screens/batches/index.vue
+++ resources/js/screens/batches/index.vue
@@ -1,204 +1,288 @@
 <script type="text/ecmascript-6">
-    export default {
-        /**
-         * The component's data.
-         */
-        data() {
-            return {
-                ready: false,
-                loadingNewEntries: false,
-                hasNewEntries: false,
-                page: 1,
-                previousFirstId: null,
-                batches: [],
-            };
-        },
+export default {
+    /**
+     * The component's data.
+     */
+    data() {
+        return {
+            ready: false,
+            loadingNewEntries: false,
+            hasNewEntries: false,
+            page: 1,
+            previousFirstId: null,
+            batches: [],
+        };
+    },
 
-        /**
-         * Prepare the component.
-         */
-        mounted() {
-            document.title = "Horizon - Batches";
+    /**
+     * Prepare the component.
+     */
+    mounted() {
+        document.title = "Horizon - Batches";
+
+        this.loadBatches();
+
+        this.refreshBatchesPeriodically();
+    },
+
+    /**
+     * Clean after the component is destroyed.
+     */
+    destroyed() {
+        clearInterval(this.interval);
+    },
+
+
+    /**
+     * Watch these properties for changes.
+     */
+    watch: {
+        '$route'() {
+            this.page = 1;
 
             this.loadBatches();
-
-            this.refreshBatchesPeriodically();
         },
+    },
 
+
+    methods: {
         /**
-         * Clean after the component is destroyed.
+         * Load the batches.
          */
-        destroyed() {
-            clearInterval(this.interval);
-        },
-
-
-        /**
-         * Watch these properties for changes.
-         */
-        watch: {
-            '$route'() {
-                this.page = 1;
-
-                this.loadBatches();
-            },
-        },
-
-
-        methods: {
-            /**
-             * Load the batches.
-             */
-            loadBatches(beforeId = '', refreshing = false) {
-                if (!refreshing) {
-                    this.ready = false;
-                }
-
-                this.$http.get(Horizon.basePath + '/api/batches?before_id=' + beforeId)
-                    .then(response => {
-                        if (!this.$root.autoLoadsNewEntries && refreshing && !response.data.batches.length) {
-                            return;
-                        }
-
-                        if (!this.$root.autoLoadsNewEntries && refreshing && this.batches.length && response.data.batches[0]?.id !== this.batches[0]?.id) {
-                            this.hasNewEntries = true;
-                        } else {
-                            this.batches = response.data.batches;
-                        }
-
-                        this.ready = true;
-                    });
-            },
-
-
-            loadNewEntries() {
-                this.batches = [];
-
-                this.loadBatches(0, false);
-
-                this.hasNewEntries = false;
-            },
-
-
-            /**
-             * Refresh the batches every period of time.
-             */
-            refreshBatchesPeriodically() {
-                this.interval = setInterval(() => {
-                    if (this.page != 1) return;
-
-                    this.loadBatches('', true);
-                }, 3000);
-            },
-
-
-            /**
-             * Load the batches for the previous page.
-             */
-            previous() {
-                this.loadBatches(
-                    this.page == 2 ? '' : this.previousFirstId
-                );
-
-                this.page -= 1;
-
-                this.hasNewEntries = false;
-            },
-
-
-            /**
-             * Load the batches for the next page.
-             */
-            next() {
-                this.previousFirstId = this.batches[0]?.id + '0';
-
-                this.loadBatches(
-                    this.batches.slice(-1)[0]?.id
-                );
-
-                this.page += 1;
-
-                this.hasNewEntries = false;
+        loadBatches(beforeId = '', refreshing = false) {
+            if (!refreshing) {
+                this.ready = false;
             }
+
+            this.$http.get(Horizon.basePath + '/api/batches?before_id=' + beforeId)
+                .then(response => {
+                    if (!this.$root.autoLoadsNewEntries && refreshing && !response.data.batches.length) {
+                        return;
+                    }
+
+                    if (!this.$root.autoLoadsNewEntries && refreshing && this.batches.length && response.data.batches[0]?.id !== this.batches[0]?.id) {
+                        this.hasNewEntries = true;
+                    } else {
+                        this.batches = response.data.batches;
+                    }
+
+                    this.ready = true;
+                });
+        },
+
+
+        loadNewEntries() {
+            this.batches = [];
+
+            this.loadBatches(0, false);
+
+            this.hasNewEntries = false;
+        },
+
+
+        /**
+         * Refresh the batches every period of time.
+         */
+        refreshBatchesPeriodically() {
+            this.interval = setInterval(() => {
+                if (this.page != 1) return;
+
+                this.loadBatches('', true);
+            }, 3000);
+        },
+
+
+        /**
+         * Load the batches for the previous page.
+         */
+        previous() {
+            this.loadBatches(
+                this.page == 2 ? '' : this.previousFirstId
+            );
+
+            this.page -= 1;
+
+            this.hasNewEntries = false;
+        },
+
+
+        /**
+         * Load the batches for the next page.
+         */
+        next() {
+            this.previousFirstId = this.batches[0]?.id + '0';
+
+            this.loadBatches(
+                this.batches.slice(-1)[0]?.id
+            );
+
+            this.page += 1;
+
+            this.hasNewEntries = false;
         }
     }
+}
 </script>
 
 <template>
     <div>
         <div class="card overflow-hidden">
-            <div class="card-header d-flex align-items-center justify-content-between">
+            <div
+                class="card-header d-flex align-items-center justify-content-between"
+            >
                 <h2 class="h6 m-0">Batches</h2>
             </div>
 
-            <div v-if="!ready" class="d-flex align-items-center justify-content-center card-bg-secondary p-5 bottom-radius">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon spin me-2 fill-text-color">
-                    <path d="M12 10a2 2 0 0 1-3.41 1.41A2 2 0 0 1 10 8V0a9.97 9.97 0 0 1 10 10h-8zm7.9 1.41A10 10 0 1 1 8.59.1v2.03a8 8 0 1 0 9.29 9.29h2.02zm-4.07 0a6 6 0 1 1-7.25-7.25v2.1a3.99 3.99 0 0 0-1.4 6.57 4 4 0 0 0 6.56-1.42h2.1z"></path>
+            <div
+                v-if="!ready"
+                class="d-flex align-items-center justify-content-center card-bg-secondary p-5 bottom-radius"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    class="icon spin me-2 fill-text-color"
+                >
+                    <path
+                        d="M12 10a2 2 0 0 1-3.41 1.41A2 2 0 0 1 10 8V0a9.97 9.97 0 0 1 10 10h-8zm7.9 1.41A10 10 0 1 1 8.59.1v2.03a8 8 0 1 0 9.29 9.29h2.02zm-4.07 0a6 6 0 1 1-7.25-7.25v2.1a3.99 3.99 0 0 0-1.4 6.57 4 4 0 0 0 6.56-1.42h2.1z"
+                    ></path>
                 </svg>
 
                 <span>Loading...</span>
             </div>
 
-
-            <div v-if="ready && batches.length == 0" class="d-flex flex-column align-items-center justify-content-center card-bg-secondary p-5 bottom-radius">
+            <div
+                v-if="ready && batches.length == 0"
+                class="d-flex flex-column align-items-center justify-content-center card-bg-secondary p-5 bottom-radius"
+            >
                 <span>There aren't any batches.</span>
             </div>
 
-            <table v-if="ready && batches.length > 0" class="table table-hover mb-0">
+            <table
+                v-if="ready && batches.length > 0"
+                class="table table-hover mb-0"
+            >
                 <thead>
-                <tr>
-                    <th>Batch</th>
-                    <th>Status</th>
-                    <th class="text-end">Size</th>
-                    <th class="text-end">Completion</th>
-                    <th class="text-end">Created</th>
-                </tr>
+                    <tr>
+                        <th>Batch</th>
+                        <th>Status</th>
+                        <th class="text-end">Size</th>
+                        <th class="text-end">Completion</th>
+                        <th class="text-end">Created</th>
+                    </tr>
                 </thead>
 
                 <tbody>
-                <tr v-if="hasNewEntries" key="newEntries" class="dontanimate">
-                    <td colspan="100" class="text-center card-bg-secondary py-2">
-                        <small><a href="#" v-on:click.prevent="loadNewEntries" v-if="!loadingNewEntries">Load New Entries</a></small>
+                    <tr
+                        v-if="hasNewEntries"
+                        key="newEntries"
+                        class="dontanimate"
+                    >
+                        <td
+                            colspan="100"
+                            class="text-center card-bg-secondary py-2"
+                        >
+                            <small
+                                ><a
+                                    href="#"
+                                    v-on:click.prevent="loadNewEntries"
+                                    v-if="!loadingNewEntries"
+                                    >Load New Entries</a
+                                ></small
+                            >
 
-                        <small v-if="loadingNewEntries">Loading...</small>
-                    </td>
-                </tr>
+                            <small v-if="loadingNewEntries">Loading...</small>
+                        </td>
+                    </tr>
 
-                <tr v-for="batch in batches" :key="batch.id">
-                    <td>
-                        <router-link class="text-decoration-none" :title="batch.id" :to="{ name: 'batches-preview', params: { batchId: batch.id }}">
-                            {{ batch.name || batch.id }}
-                        </router-link>
-                    </td>
-                    <td>
-                        <small class="badge badge-danger badge-sm" v-if="!batch.cancelledAt && batch.failedJobs > 0 && batch.totalJobs - batch.pendingJobs < batch.totalJobs">
-                            Failures
-                        </small>
-                        <small class="badge badge-success badge-sm" v-if="!batch.cancelledAt && batch.totalJobs - batch.pendingJobs == batch.totalJobs">
-                            Finished
-                        </small>
-                        <small class="badge badge-secondary badge-sm" v-if="!batch.cancelledAt && batch.pendingJobs > 0 && !batch.failedJobs">
-                            Pending
-                        </small>
-                        <small class="badge badge-warning badge-sm" v-if="batch.cancelledAt">
-                            Cancelled
-                        </small>
-                    </td>
-                    <td class="text-right text-muted">{{batch.totalJobs}}</td>
-                    <td class="text-right text-muted">{{batch.progress}}%</td>
+                    <tr v-for="batch in batches" :key="batch.id">
+                        <td>
+                            <router-link
+                                class="text-decoration-none"
+                                :title="batch.id"
+                                :to="{
+                                    name: 'batches-preview',
+                                    params: { batchId: batch.id },
+                                }"
+                            >
+                                {{ batch.name || batch.id }}
+                            </router-link>
+                        </td>
+                        <td>
+                            <small
+                                class="badge badge-danger badge-sm"
+                                v-if="
+                                    !batch.cancelledAt &&
+                                    batch.failedJobs > 0 &&
+                                    batch.totalJobs - batch.pendingJobs <
+                                        batch.totalJobs
+                                "
+                            >
+                                Failures
+                            </small>
+                            <small
+                                class="badge badge-success badge-sm"
+                                v-if="
+                                    !batch.cancelledAt &&
+                                    batch.totalJobs - batch.pendingJobs ==
+                                        batch.totalJobs
+                                "
+                            >
+                                Finished
+                            </small>
+                            <small
+                                class="badge badge-secondary badge-sm"
+                                v-if="
+                                    !batch.cancelledAt &&
+                                    batch.pendingJobs > 0 &&
+                                    !batch.failedJobs
+                                "
+                            >
+                                Pending
+                            </small>
+                            <small
+                                class="badge badge-warning badge-sm"
+                                v-if="batch.cancelledAt"
+                            >
+                                Cancelled
+                            </small>
+                        </td>
+                        <td class="text-end text-muted">
+                            {{ batch.totalJobs }}
+                        </td>
+                        <td class="text-end text-muted">
+                            {{ batch.progress }}%
+                        </td>
 
-                    <td class="text-end table-fit">
-                        {{ formatDateIso(batch.createdAt).format('YYYY-MM-DD HH:mm:ss') }}
-                    </td>
-                </tr>
+                        <td class="text-end table-fit">
+                            {{
+                                formatDateIso(batch.createdAt).format(
+                                    "YYYY-MM-DD HH:mm:ss"
+                                )
+                            }}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
 
-            <div v-if="ready && batches.length" class="p-3 d-flex justify-content-between border-top">
-                <button @click="previous" class="btn btn-secondary btn-sm" :disabled="page==1">Previous</button>
-                <button @click="next" class="btn btn-secondary btn-sm" :disabled="batches.length < 50">Next</button>
+            <div
+                v-if="ready && batches.length"
+                class="p-3 d-flex justify-content-between border-top"
+            >
+                <button
+                    @click="previous"
+                    class="btn btn-secondary btn-sm"
+                    :disabled="page == 1"
+                >
+                    Previous
+                </button>
+                <button
+                    @click="next"
+                    class="btn btn-secondary btn-sm"
+                    :disabled="batches.length < 50"
+                >
+                    Next
+                </button>
             </div>
         </div>
-
     </div>
 </template>
--- resources/js/screens/recentJobs/job-row.vue
+++ resources/js/screens/recentJobs/job-row.vue
@@ -1,22 +1,45 @@
 <template>
     <tr>
         <td>
-            <router-link class="text-decoration-none" :title="job.name" :to="{ name: $route.params.type+'-jobs-preview', params: { jobId: job.id }}">
+            <router-link
+                class="text-decoration-none"
+                :title="job.name"
+                :to="{
+                    name: $route.params.type + '-jobs-preview',
+                    params: { jobId: job.id },
+                }"
+            >
                 {{ jobBaseName(job.name) }}
             </router-link>
 
-            <small class="badge bg-secondary badge-sm" :title="`Delayed for ${delayed}`"
-                   v-if="delayed && (job.status == 'reserved' || job.status == 'pending')">
+            <small
+                class="badge bg-secondary badge-sm"
+                :title="`Delayed for ${delayed}`"
+                v-if="
+                    delayed &&
+                    (job.status == 'reserved' || job.status == 'pending')
+                "
+            >
                 Delayed
             </small>
 
-            <br>
+            <br />
 
             <small class="text-muted">
-                Queue: {{job.queue}}
+                Queue: {{ job.queue }}
 
-                <span v-if="job.payload.tags && job.payload.tags.length" class="text-break">
-                    | Tags: {{ job.payload.tags && job.payload.tags.length ? job.payload.tags.slice(0,3).join(', ') : '' }}<span v-if="job.payload.tags.length > 3"> ({{ job.payload.tags.length - 3 }} more)</span>
+                <span
+                    v-if="job.payload.tags && job.payload.tags.length"
+                    class="text-break"
+                >
+                    | Tags:
+                    {{
+                        job.payload.tags && job.payload.tags.length
+                            ? job.payload.tags.slice(0, 3).join(", ")
+                            : ""
+                    }}<span v-if="job.payload.tags.length > 3">
+                        ({{ job.payload.tags.length - 3 }} more)</span
+                    >
                 </span>
             </small>
         </td>
@@ -25,48 +48,64 @@
             {{ readableTimestamp(job.payload.pushedAt) }}
         </td>
 
-        <td v-if="$route.params.type=='completed' || $route.params.type=='silenced'" class="table-fit text-muted">
+        <td
+            v-if="
+                $route.params.type == 'completed' ||
+                $route.params.type == 'silenced'
+            "
+            class="table-fit text-muted"
+        >
             {{ readableTimestamp(job.completed_at) }}
         </td>
 
-        <td v-if="$route.params.type=='completed' || $route.params.type=='silenced'" class="table-fit text-right text-muted">
-            <span>{{ job.completed_at ? (job.completed_at - job.reserved_at).toFixed(2)+'s' : '-' }}</span>
+        <td
+            v-if="
+                $route.params.type == 'completed' ||
+                $route.params.type == 'silenced'
+            "
+            class="table-fit text-end text-muted"
+        >
+            <span>{{
+                job.completed_at
+                    ? (job.completed_at - job.reserved_at).toFixed(2) + "s"
+                    : "-"
+            }}</span>
         </td>
     </tr>
 </template>
 
 <script type="text/ecmascript-6">
-    import phpunserialize from 'phpunserialize'
-    import moment from 'moment-timezone';
+import phpunserialize from 'phpunserialize'
+import moment from 'moment-timezone';
 
-    export default {
-        props: {
-            job: {
-                type: Object,
-                required: true
+export default {
+    props: {
+        job: {
+            type: Object,
+            required: true
+        }
+    },
+
+    computed: {
+        unserialized() {
+            try {
+                return phpunserialize(this.job.payload.data.command);
+            }catch(err){
+                //
             }
         },
 
-        computed: {
-            unserialized() {
-                try {
-                    return phpunserialize(this.job.payload.data.command);
-                }catch(err){
-                    //
-                }
-            },
+        delayed() {
+            if (this.unserialized && this.unserialized.delay && this.unserialized.delay.date) {
+                return moment.tz(this.unserialized.delay.date, this.unserialized.delay.timezone)
+                    .fromNow(true);
+            } else if (this.unserialized && this.unserialized.delay) {
+                return this.formatDate(this.job.payload.pushedAt).add(this.unserialized.delay, 'seconds')
+                    .fromNow(true);
+            }
 
-            delayed() {
-                if (this.unserialized && this.unserialized.delay && this.unserialized.delay.date) {
-                    return moment.tz(this.unserialized.delay.date, this.unserialized.delay.timezone)
-                        .fromNow(true);
-                } else if (this.unserialized && this.unserialized.delay) {
-                    return this.formatDate(this.job.payload.pushedAt).add(this.unserialized.delay, 'seconds')
-                        .fromNow(true);
-                }
-
-                return null;
-            },
+            return null;
         },
-    }
+    },
+}
 </script>
