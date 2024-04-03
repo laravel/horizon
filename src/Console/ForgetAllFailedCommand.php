c9314c978f0b364a0fb2c2f554f10693482db538
Jaroslav Stefanec <55752260+jaroslavstefanec@users.noreply.github.com>
2024-04-03 08:54:32 +0200

    Created forget all failed jobs command

---
+++ src/Console/ForgetAllFailedCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Laravel\Horizon\Console;
+
+use Illuminate\Console\Command;
+use Laravel\Horizon\Contracts\JobRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'horizon:forget-all-failed-jobs')]
+class ForgetAllFailedCommand extends Command
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'horizon:forget-all-failed-jobs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete all failed jobs';
+
+    /**
+     * Execute the console command.
+     *
+     * @return null
+     */
+    public function handle(JobRepository $repository): void
+    {
+        $totalCount = $repository->totalFailed();
+
+        do {
+            collect($repository->getFailed())->pluck('id')->each(function ($failedId) use ($repository): void {
+                $repository->deleteFailed($failedId);
+
+                if ($this->laravel['queue.failer']->forget($failedId)) {
+                    $this->components->info('Failed job (id): '. $failedId .' deleted successfully!');
+                }
+            });
+        } while($repository->totalFailed() !== 0);
+
+        if($totalCount) {
+            $this->components->info('All failed jobs ('.$totalCount.') deleted successfully!');
+        } else {
+            $this->components->info('Nothing to be deleted as failed jobs are empty');
+        }
+    }
+}
