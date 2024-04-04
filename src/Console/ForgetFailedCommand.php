13652184801424f9cb09ee849b885075f7590deb
Jaroslav Stefanec <55752260+jaroslavstefanec@users.noreply.github.com>
2024-04-04 16:13:45 +0200

    Enhance forget command

--- src/Console/ForgetFailedCommand.php
+++ src/Console/ForgetFailedCommand.php
@@ -14,7 +14,7 @@ class ForgetFailedCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'horizon:forget {id : The ID of the failed job}';
+    protected $signature = 'horizon:forget {id? : The ID of the failed job} {--all : To delete all failed jobs}';
 
     /**
      * The console command description.
@@ -30,6 +30,28 @@ class ForgetFailedCommand extends Command
      */
     public function handle(JobRepository $repository)
     {
+        if ($this->option('all')) {
+            $totalFailedCount = $repository->totalFailed();
+
+            do {
+                collect($repository->getFailed())->pluck('id')->each(function ($failedId) use ($repository): void {
+                    $repository->deleteFailed($failedId);
+
+                    if ($this->laravel['queue.failer']->forget($failedId)) {
+                        $this->components->info('Failed job (id): '.$failedId.' deleted successfully!');
+                    }
+                });
+            } while ($repository->totalFailed() !== 0);
+
+            if ($totalFailedCount) {
+                $this->components->info('All failed jobs ('.$totalFailedCount.') deleted successfully!');
+            } else {
+                $this->components->info('Nothing to be deleted as failed jobs are empty');
+            }
+
+            return;
+        }
+
         $repository->deleteFailed($this->argument('id'));
 
         if ($this->laravel['queue.failer']->forget($this->argument('id'))) {
