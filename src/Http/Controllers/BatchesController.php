0f084596856d2fcdedccd3ebfe25776e248041ab
Troy Mayrand <61019450+tmayrand@users.noreply.github.com>
2024-04-25 10:20:44 -0400

    [5.x] Fix #1419 (#1424)
    * show: check for $batch
    
    * retry: check for $batch

--- src/Http/Controllers/BatchesController.php
+++ src/Http/Controllers/BatchesController.php
@@ -59,12 +59,14 @@ class BatchesController extends Controller
     {
         $batch = $this->batches->find($id);
 
-        $failedJobs = app(JobRepository::class)
-                        ->getJobs($batch->failedJobIds);
+        if ($batch) {
+            $failedJobs = app(JobRepository::class)
+                            ->getJobs($batch->failedJobIds);
+        }
 
         return [
             'batch' => $batch,
-            'failedJobs' => $failedJobs,
+            'failedJobs' => $failedJobs ?? null,
         ];
     }
 
@@ -78,14 +80,16 @@ class BatchesController extends Controller
     {
         $batch = $this->batches->find($id);
 
-        app(JobRepository::class)
-                        ->getJobs($batch->failedJobIds)
-                        ->reject(function ($job) {
-                            $payload = json_decode($job->payload);
+        if ($batch) {
+            app(JobRepository::class)
+                            ->getJobs($batch->failedJobIds)
+                            ->reject(function ($job) {
+                                $payload = json_decode($job->payload);
 
-                            return isset($payload->retry_of);
-                        })->each(function ($job) {
-                            dispatch(new RetryFailedJob($job->id));
-                        });
+                                return isset($payload->retry_of);
+                            })->each(function ($job) {
+                                dispatch(new RetryFailedJob($job->id));
+                            });
+        }
     }
 }
