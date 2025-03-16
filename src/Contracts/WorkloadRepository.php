2014f30b3077285cec4652e79f82da8dff331af1
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-03-16 09:44:12 -0400

    Update WorkloadRepository.php

--- src/Contracts/WorkloadRepository.php
+++ src/Contracts/WorkloadRepository.php
@@ -7,7 +7,7 @@ interface WorkloadRepository
     /**
      * Get the current workload of each queue.
      *
-     * @return array
+     * @return array<int, array{"name": string, "length": int, "wait": int, "processes": int, "split_queues": null|array<int, {"name": string, "wait": int, "length": int}>}>
      */
     public function get();
 }
