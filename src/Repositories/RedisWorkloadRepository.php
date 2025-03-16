9662ebb7ddb56463419b07821d219e1d961a2092
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-03-16 09:51:47 -0400

    Update RedisWorkloadRepository.php

--- src/Repositories/RedisWorkloadRepository.php
+++ src/Repositories/RedisWorkloadRepository.php
@@ -60,7 +60,7 @@ class RedisWorkloadRepository implements WorkloadRepository
     /**
      * Get the current workload of each queue.
      *
-     * @return array
+     * @return array<int, array{"name": string, "length": int, "wait": int, "processes": int, "split_queues": null|array<int, array{"name": string, "wait": int, "length": int}>}>
      */
     public function get()
     {
