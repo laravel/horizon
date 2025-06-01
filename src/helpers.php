0854b311ecdfb15427dec7660c9725bcb6439ddc
Saiful Islam <saaifulislam@gmail.com>
2025-06-02 00:49:24 +0600

    Create helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+if (function_exists('horizon_stats')) {
+    function horizon_stats()
+    {
+        $dashboard = new \Laravel\Horizon\Http\Controllers\DashboardStatsController();
+        return $dashboard->index();
+    }
+}
