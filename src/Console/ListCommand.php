73470a9f165189defe272ede2b7f9d5aafbd2d77
Amir Hossein <shokridev@gmail.com>
2025-09-27 21:56:56 +0330

    feat: add json option to ListCommand

--- src/Console/ListCommand.php
+++ src/Console/ListCommand.php
@@ -14,7 +14,8 @@ class ListCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'horizon:list';
+    protected $signature = 'horizon:list 
+                            {--json : Output all of the deployed machines information as JSON}';
 
     /**
      * The console command description.
@@ -33,6 +34,44 @@ class ListCommand extends Command
     {
         $masters = $masters->all();
 
+        $this->option('json')
+            ? $this->displayJson($masters)
+            : $this->displayForCli($masters);
+
+    }
+
+    /**
+     * Render all of the deployed machines information as JSON.
+     *
+     * @param  array  $masters
+     * @return void
+     */
+    protected function displayJson(array $masters)
+    {
+        if (empty($masters)) {
+            return $this->output->writeln('[]');
+        }
+
+        $this->output->writeln(collect($masters)->map(function ($master) {
+            return [
+                'name' => $master->name,
+                'pid' => $master->pid,
+                'supervisors' => $master->supervisors ? collect($master->supervisors)->map(function ($supervisor) {
+                    return explode(':', $supervisor, 2)[1];
+                })->implode(', ') : 'None',
+                'status' => $master->status,
+            ];
+        })->toJson());
+    }
+
+    /**
+     * Render all of the deployed machines information for the CLI.
+     *
+     * @param  array  $masters
+     * @return void
+     */
+    protected function displayForCli(array $masters)
+    {
         if (empty($masters)) {
             return $this->components->info('No machines are running.');
         }
