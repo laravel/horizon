125cc31637d8ac1d868ea2b440db6c489b63faef
Amir Hossein Shokri <shokridev@gmail.com>
2025-10-19 18:35:03 +0330

    continuation of #1613

--- src/Console/ContinueSupervisorCommand.php
+++ src/Console/ContinueSupervisorCommand.php
@@ -30,7 +30,7 @@ class ContinueSupervisorCommand extends Command
      * Execute the console command.
      *
      * @param  \Laravel\Horizon\Contracts\SupervisorRepository  $supervisors
-     * @return void
+     * @return int|void
      */
     public function handle(SupervisorRepository $supervisors)
     {
