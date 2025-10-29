8fb93e2725af0e0b1f0dd2f961e1e59435f2e8aa
Amir Hossein Shokri <shokridev@gmail.com>
2025-10-29 22:57:28 +0330

    add connection column to dashboard

--- resources/js/screens/dashboard.vue
+++ resources/js/screens/dashboard.vue
@@ -312,6 +312,7 @@
                 <thead>
                 <tr>
                     <th>Supervisor</th>
+                    <th>Connection</th>
                     <th>Queues</th>
                     <th class="text-end" style="width: 120px;">Processes</th>
                     <th class="text-end" style="width: 180px;">Balancing</th>
@@ -329,6 +330,7 @@
                         </svg>
                         {{ superVisorDisplayName(supervisor.name, worker.name) }}
                     </td>
+                    <td class="text-muted">{{ supervisor.options.connection }}</td>
                     <td class="text-muted">{{ supervisor.options.queue.replace(/,/g, ', ') }}</td>
                     <td class="text-end text-muted">{{ countProcesses(supervisor.processes) }}</td>
                     <td class="text-end text-muted" v-if="supervisor.options.balance">
