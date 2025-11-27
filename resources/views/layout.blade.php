5aa97e1c90b96e696586a564b8b41a72b9714ff0
Amir Hossein Shokri <shokridev@gmail.com>
2025-11-27 21:15:25 +0330

    fix highlight monitoring sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -59,7 +59,7 @@
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link active-class="active" to="/monitoring" class="nav-link d-flex align-items-center">
+                        <router-link :class="{ active: $route.path.startsWith('/monitoring') || ['jobs','failed'].includes($route.params.type) }" to="/monitoring" class="nav-link d-flex align-items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
                             </svg>
