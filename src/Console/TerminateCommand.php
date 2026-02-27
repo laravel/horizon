4cd7c04b5fb0562d086c732abee9f41300759ef0
webard <b.gajda@webard.me>
2026-02-27 15:19:29 +0100

    return exit code from terminate command

--- src/Console/TerminateCommand.php
+++ src/Console/TerminateCommand.php
@@ -38,7 +38,7 @@ class TerminateCommand extends Command
      * @param  \Laravel\Horizon\Contracts\MasterSupervisorRepository  $masters
      * @return void
      */
-    public function handle(CacheFactory $cache, MasterSupervisorRepository $masters)
+    public function handle(CacheFactory $cache, MasterSupervisorRepository $masters): ?int
     {
         if (config('horizon.fast_termination')) {
             $cache->forever(
@@ -50,10 +50,15 @@ class TerminateCommand extends Command
             ->filter(fn ($master) => Str::startsWith($master->name, MasterSupervisor::basename()))
             ->all();
 
-        collect(Arr::pluck($masters, 'pid'))
+        $exitCode = null;
+
+        $result = collect(Arr::pluck($masters, 'pid'))
             ->whenNotEmpty(fn () => $this->components->info('Sending TERM signal to processes.'))
-            ->whenEmpty(fn () => $this->components->info('No processes to terminate.'))
-            ->each(function ($processId) {
+            ->whenEmpty(function () use (&$exitCode) {
+                $this->components->info('No processes to terminate.');
+                $exitCode = Command::FAILURE;
+            })
+            ->each(function ($processId) use (&$exitCode) {
                 $result = true;
 
                 $this->components->task("Process: $processId", function () use ($processId, &$result) {
@@ -62,9 +67,17 @@ class TerminateCommand extends Command
 
                 if (! $result) {
                     $this->components->error("Failed to kill process: {$processId} (".posix_strerror(posix_get_last_error()).')');
+
+                    $exitCode = Command::FAILURE;
+                } else if ($exitCode === null) {
+                    $exitCode = Command::SUCCESS;
                 }
             })->whenNotEmpty(fn () => $this->output->writeln(''));
 
+
+
         $this->laravel['cache']->forever('illuminate:queue:restart', $this->currentTime());
+
+        return $exitCode;
     }
 }
