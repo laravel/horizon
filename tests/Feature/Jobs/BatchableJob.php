6dfebcc764b92e2d25db67853f4683f16e25b9a7
Steve Bauman <steven_bauman@outlook.com>
2026-04-27 10:40:43 -0400

    Revert to use multiple due to Laravel < 11 versions

--- tests/Feature/Jobs/BatchableJob.php
+++ tests/Feature/Jobs/BatchableJob.php
@@ -3,12 +3,15 @@
 namespace Laravel\Horizon\Tests\Feature\Jobs;
 
 use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class BatchableJob implements ShouldQueue
 {
-    use Queueable, Batchable;
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function handle()
     {
