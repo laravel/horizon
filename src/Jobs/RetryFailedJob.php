595843ec55302276fdfe68e615c3486ba414f1c9
Steve Bauman <steven_bauman@outlook.com>
2026-04-27 09:25:28 -0400

    Revert to master version

--- src/Jobs/RetryFailedJob.php
+++ src/Jobs/RetryFailedJob.php
@@ -10,21 +10,14 @@ use Laravel\Horizon\Contracts\JobRepository;
 class RetryFailedJob
 {
     /**
-     * The job ID.
-     *
-     * @var string
-     */
-    public $id;
-
-    /**
      * Create a new job instance.
      *
-     * @param  string  $id
+     * @param  string  $id  The job ID.
      * @return void
      */
-    public function __construct($id)
-    {
-        $this->id = $id;
+    public function __construct(
+        public $id,
+    ) {
     }
 
     /**
@@ -80,7 +73,7 @@ class RetryFailedJob
         $pushedAt = $payload['pushedAt'] ?? microtime(true);
 
         return $retryUntil
-                        ? CarbonImmutable::now()->addSeconds(ceil($retryUntil - $pushedAt))->getTimestamp()
-                        : null;
+            ? CarbonImmutable::now()->addSeconds(ceil($retryUntil - $pushedAt))->getTimestamp()
+            : null;
     }
 }
