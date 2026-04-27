b70ddb1cebca17940eac7c0cd2072126f4bcc501
Steve Bauman <steven_bauman@outlook.com>
2026-04-27 10:35:10 -0400

    Replace multiple traits with foundation Queuable

--- tests/Feature/Jobs/BatchableJob.php
+++ tests/Feature/Jobs/BatchableJob.php
@@ -3,15 +3,12 @@
 namespace Laravel\Horizon\Tests\Feature\Jobs;
 
 use Illuminate\Bus\Batchable;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Queue\Queueable;
 
 class BatchableJob implements ShouldQueue
 {
-    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Queueable, Batchable;
 
     public function handle()
     {
