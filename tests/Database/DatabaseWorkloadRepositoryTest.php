8e05d4a30e3415b36a24dbf657395b4218e6d750
Steve Bauman <steven_bauman@outlook.com>
2026-04-26 21:44:01 -0400

    Fix test

--- tests/Database/DatabaseWorkloadRepositoryTest.php
+++ tests/Database/DatabaseWorkloadRepositoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Laravel\Horizon\Tests\Database;
 
+use Illuminate\Contracts\Queue\Factory as QueueFactory;
 use Laravel\Horizon\Contracts\SupervisorRepository;
 use Laravel\Horizon\Contracts\WorkloadRepository;
 use Laravel\Horizon\Tests\Database\Fakes\SupervisorWithFakePool as Supervisor;
@@ -16,6 +17,13 @@ class DatabaseWorkloadRepositoryTest extends DatabaseTestCase
         $waitTime = Mockery::mock(WaitTimeCalculator::class);
         $waitTime->shouldReceive('calculate')->andReturn(['redis:default' => 12]);
         $this->app->instance(WaitTimeCalculator::class, $waitTime);
+
+        $queue = Mockery::mock();
+        $queue->shouldReceive('size')->with('default')->andReturn(0);
+
+        $factory = Mockery::mock(QueueFactory::class);
+        $factory->shouldReceive('connection')->with('redis')->andReturn($queue);
+        $this->app->instance(QueueFactory::class, $factory);
 
         resolve(SupervisorRepository::class)->update(
             new Supervisor(new SupervisorOptions('foo:supervisor-1', 'redis', 'default'))
