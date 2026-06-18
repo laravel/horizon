f667620e6cd4539f754c37d39271c6a9eb03900e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-06-18 12:44:09 -0400

    add helper method to register dev commands

--- src/Horizon.php
+++ src/Horizon.php
@@ -111,6 +111,19 @@ class Horizon
     }
 
     /**
+     * Helper method to register the Horizon Artisan commands in the local environment.
+     * Should be called from the `boot` method of an application service provider.
+     *
+     * @return void
+     */
+    public static function registerDevCommands()
+    {
+        if (class_exists(\Illuminate\Foundation\DevCommands::class)) {
+            \Illuminate\Foundation\DevCommands::artisan('horizon', 'horizon');
+        }
+    }
+
+    /**
      * Configure the Horizon Redis connection for a cluster.
      *
      * @param  array  $config
