f60652ce3331dad5665042b9c93a4a4c251622d7
Mior Muhammad Zaki <crynobone@gmail.com>
2026-07-24 20:55:11 +0800

    [5.x] Fix `Laravel\Horizon\RedisQueue::later()` usage on Laravel 12.10 and lower.
    fixes #1797
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/RedisQueue.php
+++ src/RedisQueue.php
@@ -3,6 +3,7 @@
 namespace Laravel\Horizon;
 
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Foundation\Application;
 use Illuminate\Queue\RedisQueue as BaseQueue;
 use Illuminate\Support\Str;
 use Laravel\Horizon\Events\JobDeleted;
@@ -112,7 +113,11 @@ class RedisQueue extends BaseQueue
     #[\Override]
     public function later($delay, $job, $data = '', $queue = null)
     {
-        $payload = (new JobPayload($this->createPayload($job, $queue, $data, $delay)))->prepare($job)->value;
+        $args = version_compare(Application::class, '12.11.0', '>=')
+            ? [$job, $queue, $data, $delay]
+            : [$job, $queue, $data];
+
+        $payload = (new JobPayload($this->createPayload(...$args)))->prepare($job)->value;
 
         if (method_exists($this, 'enqueueUsing')) {
             return $this->enqueueUsing(
