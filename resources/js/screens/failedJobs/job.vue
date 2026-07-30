1629000a2eea218fb41403e9a04ff096b97dd6be
nckrtl <nick.retel@gmail.com>
2026-07-30 23:15:45 +0200

    Use pills for retry statuses

--- resources/js/screens/failedJobs/job.vue
+++ resources/js/screens/failedJobs/job.vue
@@ -279,19 +279,18 @@
 
                 <tr v-for="retry in job.retried_by">
                     <td>
-                        <svg v-if="retry.status == 'completed'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="fill-success" style="width: 1.5rem; height: 1.5rem;">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                        </svg>
-
-                        <svg v-if="retry.status == 'reserved' || retry.status == 'pending'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="fill-warning" style="width: 1.5rem; height: 1.5rem;">
-                            <path fill-rule="evenodd" d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm5-2.25A.75.75 0 017.75 7h.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-.5a.75.75 0 01-.75-.75v-4.5zm4 0a.75.75 0 01.75-.75h.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-.5a.75.75 0 01-.75-.75v-4.5z" clip-rule="evenodd" />
-                        </svg>
-
-                        <svg v-if="retry.status == 'failed'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="fill-danger" style="width: 1.5rem; height: 1.5rem;">
-                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-                        </svg>
-
-                        <span class="ms-2">{{ upperFirst(retry.status) }}</span>
+                        <small class="badge badge-success badge-sm rounded-pill" v-if="retry.status == 'completed'">
+                            Completed
+                        </small>
+                        <small class="badge badge-danger badge-sm rounded-pill" v-if="retry.status == 'failed'">
+                            Failed
+                        </small>
+                        <small class="badge badge-info badge-sm rounded-pill" v-if="retry.status == 'reserved'">
+                            Reserved
+                        </small>
+                        <small class="badge badge-warning badge-sm rounded-pill" v-if="retry.status == 'pending'">
+                            Pending
+                        </small>
                     </td>
 
                     <td class="table-fit" :class="{ 'horizon-linked-cell': retry.status == 'failed' }">
