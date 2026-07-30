6e970b97998416fc3b7de040cc13135e3aba43e4
nckrtl <nick.retel@gmail.com>
2026-07-30 13:20:07 +0200

    Unify job and batch list controls

--- resources/js/screens/batches/index.vue
+++ resources/js/screens/batches/index.vue
@@ -191,8 +191,8 @@
             <div class="card-header d-flex align-items-center justify-content-between">
                 <h2 class="h6 m-0">Batches</h2>
 
-                <div class="form-control-with-icon" v-if="available">
-                    <div class="icon-wrapper">
+                <div class="horizon-search-control" v-if="available">
+                    <div class="horizon-search-control-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon">
                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
                         </svg>
--- resources/js/screens/failedJobs/index.vue
+++ resources/js/screens/failedJobs/index.vue
@@ -158,20 +158,6 @@
                 return `Total retries: ${job.retried_by.length}, Last retry status: ${this.upperFirst(lastRetry.status)}`;
             },
 
-            /**
-             * Determine the badge variant for the latest retry.
-             */
-            retriedJobBadgeClass(job) {
-                let lastRetry = job.retried_by[job.retried_by.length - 1];
-
-                return {
-                    completed: 'badge-success',
-                    failed: 'badge-danger',
-                    pending: 'badge-retry',
-                    reserved: 'badge-retry',
-                }[lastRetry.status] ?? 'badge-warning';
-            },
-
 
             /**
              * Poll handler to refresh the jobs at regular intervals.
@@ -219,8 +205,8 @@
             <div class="card-header d-flex align-items-center justify-content-between">
                 <h2 class="h6 m-0">Failed Jobs</h2>
 
-                <div class="failed-jobs-search-control">
-                    <div class="failed-jobs-search-icon">
+                <div class="horizon-search-control">
+                    <div class="horizon-search-control-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon">
                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
                         </svg>
@@ -270,8 +256,7 @@
                         <router-link class="horizon-row-link horizon-cell-link" :title="job.name" :to="{ name: 'failed-jobs-preview', params: { jobId: job.id }}">
                             {{ jobBaseName(job.name) }}
 
-                            <small class="horizon-row-action ms-1 badge badge-sm rounded-pill"
-                                   :class="retriedJobBadgeClass(job)"
+                            <small class="horizon-row-action ms-1 badge badge-danger badge-sm rounded-pill"
                                    :title="retriedJobTooltip(job)"
                                    v-if="wasRetried(job)">
                                 Retried
